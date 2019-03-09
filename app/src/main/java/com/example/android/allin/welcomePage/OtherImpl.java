a548564f1f55709424d42ad1a08dda59231ba29d
donmikele <kaczynskimichalkonrad@gmail.com>
2019-03-09 01:30:06 +0100

    First step of refactor

---
+++ app/src/main/java/com/example/android/allin/welcomePage/OtherImpl.java
@@ -0,0 +1,36 @@
+package com.example.android.allin.welcomePage;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.android.allin.R;
+import com.example.android.allin.welcomePage.Other;
+import com.example.android.allin.welcomePage.other.otherPage.CalculateIp;
+import com.example.android.allin.welcomePage.other.otherPage.Notepad;
+import com.example.android.allin.welcomePage.other.otherPage.YourNotes;
+
+public class OtherImpl extends AppCompatActivity implements Other {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+    }
+
+    public void openNotepad(View view) {
+        Intent intent = new Intent(this, Notepad.class);
+        startActivity(intent);
+    }
+
+    public void openYourNotes(View view) {
+        Intent intent = new Intent(this,YourNotes.class);
+        startActivity(intent);
+    }
+
+    public void openCalculateIp(View view) {
+        Intent intent = new Intent(this,CalculateIp.class);
+        startActivity(intent);
+    }
+}
